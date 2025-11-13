17534c1560ce28e0e6af677e07d63d73c28ff8c5
unknown <beebee-project>
2025-11-13 22:38:38 +0900

    update

--- services/emailService.js
+++ services/emailService.js
@@ -1,12 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
-  connectionTimeout: 10_000,
-  socketTimeout: 10_000,
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 const sendVerificationEmail = async (to, token) => {
