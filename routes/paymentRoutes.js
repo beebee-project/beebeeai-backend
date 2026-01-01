ae3866775d196dc0f046dc701ec09643bbe4fdce
unknown <beebee-project>
2026-01-01 22:52:43 +0900

    update

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,13 +3,6 @@ const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 const paymentController = require("../controllers/paymentController");
 
-// 사용량/플랜 (프론트에서 계속 호출 중)
-router.get("/usage", protect, paymentController.getUsage);
-router.get("/plans", paymentController.getPlans);
-
-// (현재 프론트 결제 버튼이 호출하는 엔드포인트)
-router.post("/checkout", protect, paymentController.createCheckout);
-
 // 정기결제 시작: customerKey 기반으로 Toss 결제창(빌링) 오픈
 router.post(
   "/subscription/start",
